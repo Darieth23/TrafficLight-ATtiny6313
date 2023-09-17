c3d2c8788aa7170cf5891e8c95af62e234349185
Xuthyst <darieth.fonseca@ucr.ac.cr>
2023-09-16 21:11:10 -0600

    version 2

--- src/blink.c
+++ src/blink.c
@@ -1,16 +1,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
-//estados
-#define PasoVehi 0 //auto detenido luz roja
-#define ParpadeoVehi 1 //auto detenido luz parpadea
-#define AltoVehi 2 //auto en movimiento luz verde
-#define PasoPeaton 3 //peaton en movimiento luz verde
-#define ParpadeoPeaton 4 //peaton detenido luz parpadea
-#define AltoPeaton 5 //peaton detenido luz roja
-unsigned int estado;
-unsigned int boton;
+int estado;
+int boton;
 int main(void)
 {
   DDRB = 0x3F; //Configuracion del puerto. B3 como salida
@@ -18,66 +10,98 @@ int main(void)
   estado = 0; //Alto
   boton = 0;
   //int tiempo = 0;
-  GIMSK |= (1 << INT0); 
   MCUCR |= (1 << ISC00); 
   MCUCR |= (1 << ISC01);
-  GIMSK |= (1 << INT1); 
   MCUCR |= (1 << ISC10); 
   MCUCR |= (1 << ISC11);
   MCUCR |= (1 << PUD);
+  GIMSK |= (1 << INT1); 
+  GIMSK |= (1 << INT0); 
   sei();
   while (1) {
-    if(estado == 0){
-      if(boton == 0){
-        estado = 0;
-        PORTB = 0x1A;
-      }
-      else{
-        estado = 1;         
-      }
-    }  
-    else if(estado == 1){
-        estado = 2; 
-        PORTB = 0x1A;
-        _delay_ms(5000);
-        PORTB = 0x0A;
-        _delay_ms(5000);
-        PORTB = 0x1A;
-        _delay_ms(5000);
-        PORTB = 0x0A;
-        _delay_ms(5000);
-      }
-      else if(estado == 2){
-        estado = 3;
-        PORTB = 0x2A;
-        _delay_ms(10000);
-      }
-      else if(estado == 3){
-        estado = 4;
-        PORTB = 0x25;
-        _delay_ms(50000);
-      }
-      else if(estado == 4){
-        estado = 5;
-        PORTB = 0x25;
-        _delay_ms(5000);
-        PORTB = 0x20;
-        _delay_ms(5000);
-        PORTB = 0x25;
-        _delay_ms(5000);
-        PORTB = 0x20;
-        _delay_ms(5000); 
-      }      
-      else if(estado == 5){
-        estado = 0;
-        boton = 0;
-        PORTB = 0x2A;
-        _delay_ms(10000);
-      }
+    switch(estado){
+      case 0:
+        if(boton == 0){
+          estado = 0;
+          PORTB = 0x1A;
+          break;
+        }
+        if(boton == 1){
+          estado = 1;
+          break;         
+        }
+      case 1:
+        if(1){
+          estado = 2; 
+          PORTB = 0x1A;
+          _delay_ms(5000);
+          PORTB = 0x0A;
+          _delay_ms(5000);
+          PORTB = 0x1A;
+          _delay_ms(5000);
+          PORTB = 0x0A;
+          _delay_ms(5000);
+          break;        
+        }
+        else{
+          break;
+          //falta
+        }
+      case 2:
+        if(1){
+          estado = 3;
+          PORTB = 0x2A;
+          _delay_ms(10000);
+          break; 
+        }
+        else{
+          break;
+          //falta
+        }
+      case 3:
+        if(1){
+          estado = 4;
+          PORTB = 0x25;
+          _delay_ms(50000);          
+          break;
+        }
+        else{
+          break;
+          //falta
+        }
+      case 4:
+        if(1){
+          estado = 5;
+          PORTB = 0x25;
+          _delay_ms(5000);
+          PORTB = 0x20;
+          _delay_ms(5000);
+          PORTB = 0x25;
+          _delay_ms(5000);
+          PORTB = 0x20;
+          _delay_ms(5000);
+          break;
+        }
+        else{
+          break;
+          //falta
+        }
+      case 5:
+        if(1){
+          estado = 0;
+          boton = 0;
+          PORTB = 0x2A;
+          _delay_ms(10000);
+          break;
+        }
+        else{
+          break;
+          //falta
+        }        
+    }
   }
 }
 ISR(INT0_vect){
   boton = 1;
-
 }
 ISR (INT1_vect, ISR_ALIASOF (INT0_vect));
